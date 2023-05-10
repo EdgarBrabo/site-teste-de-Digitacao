3173242b68397a1ca40d9a4e58e6ed56131926b6
Edgar Brabo <wallacebrabo21@gmail.com>
2023-05-09 23:15:54 -0300

    add: funçao alterar temao ao carregar a pagina

--- scripts.js
+++ scripts.js
@@ -6,12 +6,21 @@ const historico = document.querySelector("#historico");
 const alternarTemaBtn = document.querySelector("#alternarTema");
 
 const textos = [
-    "Exemplo de texto para digitar.",
-    "Outro exemplo de texto para digitar.",
-    "Mais um exemplo de texto para digitar",
-    "Digite isso.", "Você pode digitar isso aqui.",
-    "oi", "Três tigres", "Gato de botas", "Guarda-chuva",
-    "Teclado",
+    "Eu amo comer pizza", "Estou animado para a viagem",
+    "Ela é uma ótima amiga", "O sol está brilhando hoje", 
+    "Vamos ao cinema amanhã", "Ele é um excelente jogador de futebol", 
+    "Preciso fazer compras no supermercado", "Estou com saudades da minha família", 
+    "Adoro passear no parque", "Quero aprender a tocar guitarra", "Estou cansado de estudar", 
+    "Vou fazer uma pausa para o café", "Que filme interessante!", "Vamos jantar fora esta noite", 
+    "A festa foi muito divertida", "Gosto de ler antes de dormir", 
+    "Preciso resolver esse problema urgentemente", "Que linda paisagem!", 
+    "Adoro ouvir música no carro", "Vou tirar um cochilo depois do almoço", 
+    "Ela é uma excelente cozinheira", "Vamos marcar um encontro", "Estou ansioso pelo fim de semana", 
+    "Adoro viajar e conhecer novos lugares", "Preciso renovar meu guarda-roupa", 
+    "Que delícia esse bolo de chocolate!", "Vou dar um passeio no parque", 
+    "Estou com frio, preciso de um casaco", "Gosto de nadar no mar", 
+    "Vamos organizar uma festa surpresa", "Quero aprender a dançar salsa", 
+    "Eles são um casal muito fofo"
 ];
 
 function novoTexto() {  
@@ -64,16 +73,38 @@ function reiniciarTeste(){
     historico.innerHTML ="";
 }
 
+function adicionarLocaStorage(chave, valor){
+
+    localStorage.setItem(chave, valor)
+}
+
 function alternarTema(){
     const body = document.body
 
     body.classList.toggle("claro");
     body.classList.toggle("escuro");
+
+    const tema = body.classList.contains('claro') ? 'claro' : 'escuro'
+    adicionarLocaStorage("tema", tema)
 }
+
+window.addEventListener("load", function() {
+    const tema = localStorage.getItem("tema")
+    if(tema === null) return
+
+    if(tema === "claro"){
+        document.body.classList.remove("escuro");
+        document.body.classList.add("claro");
+      } else {
+        document.body.classList.remove('claro')
+        document.body.classList.add('escuro')
+      }
+
+})
+
 
 entrada.addEventListener("keyup", atualizarTeste);
 reiniciar.addEventListener("click", reiniciarTeste)
-
 alternarTemaBtn.addEventListener("click", alternarTema);
 
 novoTexto();
